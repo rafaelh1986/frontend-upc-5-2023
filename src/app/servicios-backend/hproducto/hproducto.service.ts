1fcfc9c241f56a9bdf5ee978f1c58f074be79d75
Rafael Heredia Padilla <rafaelherediap@gmail.com>
2023-06-21 21:29:24 -0400

    tab4

--- src/app/servicios-backend/hproducto/hproducto.service.ts
+++ src/app/servicios-backend/hproducto/hproducto.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HproductoService {
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
 
-  constructor() { }
+  URL_GET_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto"
+  URL_GET_BY_ID = this.PATH_BACKEND + "/api/HProducto/GetHProductoById"
+  URL_ADD_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/AddHProducto"
+  URL_UPDATE_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/UpdateHProducto"
+  URL_DELETE_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/DeleteHProducto"
+  constructor(private http: HttpClient) { }
+  public GetHProducto(): Observable<HttpResponse<any>> {
+
+    return this.http
+        .get<any>(this.URL_GET_HPRODUCTO,
+            { observe: 'response' })
+        .pipe();
+  }
+
+  public AddHProducto(entidad): Observable<HttpResponse<any>> {
+
+      return this.http
+          .post<any>(this.URL_ADD_HPRODUCTO, entidad,
+              { observe: 'response' })
+          .pipe();
+  }
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../servicios-backend/productos/productos.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page {
+  public listHProducto = [];
+  public idHProducto = ""
+  public cantidad = ""
+  public idProducto = ""
+  public idCarritoCompra = ""
+  public swGuardarCambios = false
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productosService: ProductosService) {
+    this.GetHProducto();//Se carga el listado cada vez que se abra la pag.
   }
+
+  public GetHProducto(){
+    this.productosService.GetProductos().subscribe({
+        next: (response: HttpResponse<any>) => {
+            this.listHProducto = response.body;
+            //console.log(this.listProducto)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            console.log('complete - this.GetHProducto()');
+        },
+    });
+  }  
 
 }
