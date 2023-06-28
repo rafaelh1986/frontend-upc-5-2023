c72027ec21e576dd7715c24c3119e3799a7e6407
Rafael Heredia Padilla <rafaelherediap@gmail.com>
2023-06-27 22:25:56 -0400

    Agregando tabs 6 y 7

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -58,6 +58,26 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'tab6',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab6/tab6.module').then(m => m.Tab6PageModule)
+          }
+        ]
+      },
+      {
+        path: 'tab7',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab7/tab7.module').then(m => m.Tab7PageModule)
+          }
+        ]
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
