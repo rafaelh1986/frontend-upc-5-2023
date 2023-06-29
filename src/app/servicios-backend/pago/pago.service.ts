fd8fe56b7485cedefddced45a2a9693d9242ae9e
Rafael Heredia Padilla <rafaelherediap@gmail.com>
2023-06-29 02:26:00 -0400

    Front tab7

--- src/app/servicios-backend/pago/pago.service.ts
+++ src/app/servicios-backend/pago/pago.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagoService {
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
 
-  constructor() { }
+  URL_GET_PAGO = this.PATH_BACKEND + "/api/Pago"
+  URL_GET_BY_ID_PAGO = this.PATH_BACKEND + "/api/Pago/GetPagoById"
+  URL_ADD_PAGO = this.PATH_BACKEND + "/api/Pago/AddPago"
+  URL_UPDATE_PAGO = this.PATH_BACKEND + "/api/Pago/UpdatePago"
+  URL_DELETE_PAGO = this.PATH_BACKEND + "/api/Pago/DeletePago"
+
+  constructor(private http: HttpClient) { }
+  public GetPagos(): Observable<HttpResponse<any>> {
+
+    return this.http
+        .get<any>(this.URL_GET_PAGO,
+            { observe: 'response' })
+        .pipe();
+  }
+
+  public AddPago(entidad): Observable<HttpResponse<any>> {
+
+      return this.http
+          .post<any>(this.URL_ADD_PAGO, entidad,
+              { observe: 'response' })
+          .pipe();
+  }
+
+  public UpdatePago(entidad): Observable<HttpResponse<any>> {
+
+      return this.http
+          .post<any>(this.URL_UPDATE_PAGO, entidad,
+              { observe: 'response' })
+          .pipe();
+  }
+
+  public DeletePago(item): Observable<HttpResponse<any>> {
+    
+    let params = new HttpParams();
+    params = params.set('id', item.id);
+
+    return this.http
+        .post<any>(this.URL_DELETE_PAGO,  "", {params: params, observe: 'response' })
+        .pipe();
+  }
 }
--- src/app/tab7/tab7.page.ts
+++ src/app/tab7/tab7.page.ts
@@ -1,15 +1,127 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { PagoService } from '../servicios-backend/pago/pago.service';
 @Component({
   selector: 'app-tab7',
   templateUrl: './tab7.page.html',
   styleUrls: ['./tab7.page.scss'],
 })
-export class Tab7Page implements OnInit {
+export class Tab7Page{
+  public listPago = [];
+  public idPago = ""
+  public fecha = ""
+  public monto = ""
+  public idAlumno = ""
+  public swGuardarCambios = false
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private pagoService: PagoService) {
+    this.GetPagos();//Se carga el listado cada vez que se abra la pag.
   }
+
+  public GetPagos(){
+    this.pagoService.GetPagos().subscribe({
+        next: (response: HttpResponse<any>) => {
+            this.listPago = response.body;
+            console.log(this.listPago)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            console.log('complete - this.GetPagos()');
+        },
+    });
+  }
+
+  public addPago(){
+      if (this.monto.length > 0 && this.idAlumno.length > 0) {
+          var entidad = {
+              monto : this.monto,
+              idAlumno : this.idAlumno
+          }
+          console.log(entidad)
+          this.pagoService.AddPago(entidad).subscribe({
+              next: (response: HttpResponse<any>) => {
+                  console.log(response.body)//1
+                  if(response.body == 1){
+                      alert("Se agrego el Pago con exito :)");
+                      this.GetPagos();//Se actualize el listado
+                      this.monto = "";
+                      this.idAlumno = "";
+                  }else{
+                      alert("Al agregar el Pago fallo exito :(");
+                  }
+              },
+              error: (error: any) => {
+                  console.log(error);
+              },
+              complete: () => {
+                  console.log('complete - this.addPago()');
+              },
+          });
+      }
+  }
+
+  public guardarCambios(){
+    this.swGuardarCambios = false;
+    if (this.monto.length > 0 && this.idAlumno.length > 0) {
+        var entidad = {
+            id: this.idPago,
+            monto : this.monto,
+            idAlumno: this.idAlumno
+        }
+        console.log(entidad)
+        this.pagoService.UpdatePago(entidad).subscribe({
+            next: (response: HttpResponse<any>) => {
+                console.log(response.body)//1
+                if(response.body == 1){
+                    alert("Se modifico el Pago con exito :)");
+                    this.GetPagos();//Se actualize el listado
+                    this.idPago = "";
+                    this.monto = "";
+                    this.idAlumno = "";
+                }else{
+                    alert("Al modificar el Pago fallo exito :(");
+                }
+            },
+            error: (error: any) => {
+                console.log(error);
+            },
+            complete: () => {
+                console.log('complete - this.guardarCambios()');
+            },
+        });
+    }
+}
+
+  public updatePago(item){
+    console.log(item)
+    this.idPago = item.id //oculto
+    this.monto = item.monto //input
+    this.idAlumno = item.idAlumno //input
+    this.swGuardarCambios = true;
+  }
+
+  public deletePago(item){
+        console.log(item.id)
+        this.pagoService.DeletePago(item).subscribe({
+            next: (response: HttpResponse<any>) => {
+                console.log(response.body)//1
+                if(response.body == 1){
+                    alert("Se elimino el Pago con exito :)");
+                    this.GetPagos();//Se actualize el listado
+                }else{
+                    alert("Al eliminar el Pago fallo exito :(");
+                }
+            },
+            error: (error: any) => {
+                console.log(error);
+            },
+            complete: () => {
+                console.log('complete - this.deletePago()');
+            },
+        });
+  }
+
 
 }
